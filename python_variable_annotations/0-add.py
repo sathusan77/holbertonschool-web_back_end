92a539678a3ef6c32773ec4b6337ee0583c08d63
jeyakumar sathusan <jeyakumar.sathusan@gmail.com>
2026-04-14 01:51:41 +0200

    o

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that contains a type-annotated add function."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return their sum."""
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that contains a type-annotated concat function."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings and return the result."""
+    return str1 + str2
